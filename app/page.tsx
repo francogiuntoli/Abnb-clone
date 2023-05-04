b4c3c8e718d6fecbdecd0e41b1f221258f25ff6d
FdgCertainly <94852583+FdgCertainly@users.noreply.github.com>
2023-05-04 18:48:24 +0200

    test to deploy 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import getCurrentUser from "./actions/getCurrentUser"
 import getListings, { IListingsParams } from "./actions/getListings"
 import ClientOnly from "./components/ClientOnly"
@@ -16,20 +14,38 @@ export default async function Home({ searchParams }: HomeProps) {
   const currentUser = await getCurrentUser()
 
   if (listings.length === 0) {
-    return <EmptyState showReset />
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
   }
 
   return (
-    <Container>
-      <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-        {listings.map((listing) => (
-          <ListingCard
-            currentUser={currentUser}
-            key={listing.id}
-            data={listing}
-          />
-        ))}
-      </div>
-    </Container>
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+            grid
+            grid-cols-1 
+            gap-8 
+            pt-24 
+            sm:grid-cols-2 
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
