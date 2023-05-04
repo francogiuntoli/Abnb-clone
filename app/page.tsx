777bb374061de015186229b031ba7071acd9d101
FdgCertainly <94852583+FdgCertainly@users.noreply.github.com>
2023-05-04 18:47:11 +0200

    test for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import getCurrentUser from "./actions/getCurrentUser"
 import getListings, { IListingsParams } from "./actions/getListings"
 import ClientOnly from "./components/ClientOnly"
@@ -18,18 +20,16 @@ export default async function Home({ searchParams }: HomeProps) {
   }
 
   return (
-    <ClientOnly>
-      <Container>
-        <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-          {listings.map((listing) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
-        </div>
-      </Container>
-    </ClientOnly>
+    <Container>
+      <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {listings.map((listing) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
   )
 }
