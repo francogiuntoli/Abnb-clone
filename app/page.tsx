3bbd2b3140a875db2fa07db4ccef674a072c3fb2
FdgCertainly <94852583+FdgCertainly@users.noreply.github.com>
2023-05-04 18:54:54 +0200

    next error check

--- app/page.tsx
+++ app/page.tsx
@@ -14,38 +14,20 @@ export default async function Home({ searchParams }: HomeProps) {
   const currentUser = await getCurrentUser()
 
   if (listings.length === 0) {
-    return (
-      <ClientOnly>
-        <EmptyState showReset />
-      </ClientOnly>
-    )
+    return <EmptyState showReset />
   }
 
   return (
-    <ClientOnly>
-      <Container>
-        <div
-          className="
-            grid
-            grid-cols-1 
-            gap-8 
-            pt-24 
-            sm:grid-cols-2 
-            md:grid-cols-3
-            lg:grid-cols-4
-            xl:grid-cols-5
-            2xl:grid-cols-6
-          "
-        >
-          {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
-        </div>
-      </Container>
-    </ClientOnly>
+    <Container>
+      <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {listings.map((listing) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
   )
 }
