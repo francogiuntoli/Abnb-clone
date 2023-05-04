ba8d56cf959bfda8ed9bf44f80fae390250838e9
FdgCertainly <francogiuntoli@gmail.com>
2023-05-04 21:59:52 +0200

    syntax reformat

--- app/components/inputs/CategoryInput.tsx
+++ app/components/inputs/CategoryInput.tsx
@@ -18,8 +18,9 @@ export default function CategoryInput({
   return (
     <div
       onClick={() => onClick(label)}
-      className={`flex cursor-pointer flex-col gap-3 rounded-xl border-2 p-4 transition hover:border-black
-       ${selected ? "border-black" : "border-neutral-200"}`}
+      className={`flex cursor-pointer flex-col gap-3 rounded-xl border-2 p-4 transition hover:border-black ${
+        selected ? "border-black" : "border-neutral-200"
+      }`}
     >
       <Icon size={30} />
       <div className="font-semibold"> {label}</div>
